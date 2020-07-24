767673390eea3e909382a530b2405585e2ea4788
Siddhartha Pratap Singh <62471966+sps1112@users.noreply.github.com>
2020-07-24 19:10:40 +0530

    add collectibles with spawner and ui

---
+++ Assets/Scripts/CollectibleSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleSpawner : MonoBehaviour
+{
+    public GameObject[] collectibles;
+
+    int index = 0;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Point")
+        {
+            SetCollectible(other.gameObject.transform.position);
+        }
+        else
+        {
+            Debug.Log(other.gameObject.tag + "  " + other.gameObject.name);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+    }
+
+    void SetCollectible(Vector3 spawnPosition)
+    {
+        collectibles[index].transform.position = spawnPosition;
+        collectibles[index].SetActive(true);
+        index++;
+        index %= collectibles.Length;
+    }
+}
---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreUI : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        SetScore(0);
+    }
+
+    public void SetScore(float score)
+    {
+        scoreText.text = "Score: " + ((int)score).ToString();
+    }
+}
